34bfe682dd763ab6d11f1c4d195231428dd3ae91
eyal <eyal.regular@gmail.com>
2016-01-14 01:45:43 +0200

    first init

---
+++ app.js
@@ -0,0 +1,17 @@
+var backend = 'http://queue-backend.s3-website-eu-west-1.amazonaws.com/';
+
+var xhttp = new XMLHttpRequest();
+
+xhttp.onreadystatechange = function() {
+	if (xhttp.readyState == 4) {
+		document.getElementById('position').innerHTML = xhttp.responseText;
+	}
+};
+
+xhttp.open('GET', backend, true);
+xhttp.send();
+
+setInterval(function() {
+	xhttp.open('GET', backend, true);
+	xhttp.send();
+}, 10000);
